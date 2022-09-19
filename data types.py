734e9f5051aaf01c6fd478b16dfe496d216843ec
abhilekha2506 <113585859+abhilekha2506@users.noreply.github.com>
2022-09-19 15:44:29 +0530

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,81 @@
+int
+float
+str
+list
+tuple
+set
+dict
+bool
+bytes
+bytearray
+complex
+
+# print(type(6))
+# print(type(6.7))
+# print(type(0))
+# print(type(0.4))
+
+# print(type('hi'))
+# print(type("hi"))
+# print(type('''hi'''))
+# print(type("""hi"""))
+
+# print(type([]))
+# print(type([1,2,3]))
+# print(type(['hi','hello']))
+
+# print(type(True))
+# print(type(False))
+
+# a={56}
+# print(type(a))
+
+# b={1:'python'}
+# print(type(b))
+
+# c=[1,2]
+# c.clear
+# print(c)
+
+# d={2,3,4}
+# d.clear
+# print(d)
+
+# e={2:'hi'}
+# e.clear
+# print(e)
+
+# print(type(b'hi'))
+
+# print(bytearray(b'hi'))
+
+# print(type(2+3j))
+# print(type(8j))
+
+# print(86)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
