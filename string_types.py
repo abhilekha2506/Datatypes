ff6eae6af216786109f282297f9fd742bcdc6872
abhilekha2506 <113585859+abhilekha2506@users.noreply.github.com>
2022-09-19 15:45:55 +0530

    Add files via upload

---
+++ string_types.py
@@ -0,0 +1,54 @@
+# print('hello'*5)   hellohellohellohellohello
+# print('hello '* 5)    hello hello hello hello hello 
+# print('he\n'*5,end='')
+
+# print('hi' * 4.5)
+
+# print('hi' * 'b')
+
+# print('hi' + 4)
+# print('hi' + 3.7)
+
+# print('hi'+'bye')
+# print('hi '+'bye')
+# print('hi'+' bye')
+# print('hi'+''+'bye')
+# print('hi'+' '+'bye')
+# print('hi'+'                    '+'bye')
+
+# print('hi'+'-'+'5')
+
+# print('hello' 'bye')
+# print('hello' ,'bye')
+# print('hello'+'  '+'bye')
+# print('hello'+'  '+str(8))
+
+# from pdb import pm
+
+
+# today is thursday time is 5:00 pm
+
+# print('today is thursday time is 5:00pm')
+# print('today is thursday time is 5:00pm')
+# print('''today is thursday time is 5:00pm''')
+# print("""today is thursday time is 5:00pm""")
+# print('today'+' '+'is'+' '+'thursday'+' '+'time'+' '+'is'+' '+'5:00pm')
+
+# print(1,2,3,4,5,6,7)
+# print('1'+' '+'2'+' '+'3')
+
+# print(1,2,3,4,5,sep='')
+# print(1,2,3,4,5,sep=' ')
+# print(1,2,3,4,5,sep='  ')
+# print(1,2,3,4,5,sep='-')
+
+
+
+
+
+
+
+
+
+
+
