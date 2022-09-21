f326cf1d97d45f3ef31b6d289361fd95d6523edf
abhilekha2506 <113585859+abhilekha2506@users.noreply.github.com>
2022-09-21 12:15:36 +0530

    Add files via upload

---
+++ format_specifier.py
@@ -0,0 +1,92 @@
+# note:-
+# int --> %d or %int
+# float  --> %f
+# str  --> %s
+
+a=5 ; b=5.7 ; c='hello'
+print(type(a),type(b),type(c))
+print(a)
+print('%d' %a)
+print('%i' %a)
+print(b)
+print(b)
+print('%f' %b)
+print('%.1f'%b)
+print('%.2f'%b)
+print('%.3f'%b)
+
+a=6
+print(float(a))
+print(str(a))
+print('%f' %a)
+print('%.1' %a)
+
+b=4.5
+print(int(b))
+print(str(b))
+
+c='hello'
+print(int(c))
+print(float(c))
+
+d='5'
+print(type(d))
+e=int(d)
+print(e,type(e))
+f=float(d)
+print(f,type(f))
+
+g='5.6'
+print(type(g))
+print(int(g))
+
+h=float(g)
+print(h,type(h))
+i=int(h)
+print(i,type(i))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
